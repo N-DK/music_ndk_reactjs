567fda6db0d449428d7922d0c058205d8645a671
N_DK <nguyenkhoadang135759@gmail.com>
2023-09-22 18:55:07 +0700

    edit component CardSongItem

--- src/components/CardSongItem/index.js
+++ src/components/CardSongItem/index.js
@@ -1,5 +1,8 @@
+import { faCirclePlay, faHeart } from '@fortawesome/free-regular-svg-icons';
 import styles from './CardSongItem.module.scss';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -7,17 +10,50 @@ function CardSongItem() {
     return (
         <div className="col-2-4">
             <div className={`${cx('wrapper')} overflow-hidden rounded-3 mb-3`}>
-                <div>
+                <div
+                    className={`${cx(
+                        'thumbnail__container',
+                    )} overflow-hidden position-relative`}
+                >
                     <img
                         src="https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/d/5/8/a/d58aa48a38c0a8dc89c95277b456bc75.jpg"
                         alt=""
                         className="w-100 h-100"
                     />
+                    <div
+                        className={`${cx(
+                            'action__container',
+                        )} position-absolute w-100 h-100 start-0 top-0 d-flex align-items-center justify-content-center`}
+                    >
+                        <a
+                            href=""
+                            className="text-white rounded-circle d-flex align-items-center is-hover-circle justify-content-center square_30"
+                        >
+                            <FontAwesomeIcon icon={faHeart} />
+                        </a>
+                        <a
+                            href=""
+                            className="fs-1 ms-4 me-4 text-white rounded-circle d-flex align-items-center justify-content-center square_30"
+                        >
+                            <FontAwesomeIcon icon={faCirclePlay} />
+                        </a>
+                        <a
+                            href=""
+                            className="text-white rounded-circle d-flex align-items-center is-hover-circle justify-content-center square_30"
+                        >
+                            <FontAwesomeIcon icon={faEllipsis} />
+                        </a>
+                    </div>
                 </div>
                 <div
                     className={` text-center pt-4 pb-4 border rounded-3 border-top-0 rounded-top-0 f-family`}
                 >
-                    <h6 className="mb-1">Cheese (Single)</h6>
+                    <a
+                        href="#"
+                        className="mb-1 d-block text-decoration-none text-black"
+                    >
+                        Cheese (Single)
+                    </a>
                     <a href="#" className="subtitle_color  is_truncate">
                         Cravity
                     </a>
---
+++ src/pages/Artist/index.js
@@ -0,0 +1,9 @@
+function Artist() {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Artist;
